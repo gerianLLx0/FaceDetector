1a45d29240c44a290bdffcbe0ee5be3174d09b44
x0g <gerianllanes@gmail.com>
2023-04-08 22:52:14 +1000

    Replace haar detector with YuNet DNN detector

--- face_detector.py
+++ face_detector.py
@@ -22,7 +22,7 @@ import numpy as np
 import cv2 as cv
 
 
-def process_webcam(face_cascade):
+def process_webcam(detector, tm):
     """
     webcam video capture code from opencv docs
     https://docs.opencv.org/4.x/dd/d43/tutorial_py_video_display.html
@@ -38,56 +38,48 @@ def process_webcam(face_cascade):
         if not ret:
             print("Can't receive frame (stream end?). Exiting ...")
             break
-        
-        # # Our operations on the frame come here
-        # gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        # # Display the resulting frame
-        # cv.imshow('frame', gray)
-        detectAndDisplay(frame, face_cascade)
-        
-        # how to exit
+        # Operations on frame
+        img_H = int(frame.shape[0])
+        img_W = int(frame.shape[1])
+        detector.setInputSize((img_W, img_H))
+        # Get detections
+        tm.start()
+        faces = detector.detect(frame)
+        tm.stop()
+        # Draw results on the input image
+        draw_on_frame(frame, faces, tm.getFPS())
+        cv.imshow('Live', frame)
+        # How to exit
         if cv.waitKey(1) == ord('q'):
             break
-    # When everything done, release the capture
+    # When done, release the capture
     cap.release()
     cv.destroyAllWindows()
     
-    
-def detectAndDisplay(frame, face_cascade):
-    frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    frame_gray = cv.equalizeHist(frame_gray)
-    #-- Detect faces
-    faces = face_cascade.detectMultiScale(frame_gray)
-    for (x,y,w,h) in faces:
-        center = (x + w//2, y + h//2)
-        frame = cv.ellipse(frame, center, (w//2, h//2), 0, 0, 360, (255, 0, 255), 4)
-        # faceROI = frame_gray[y:y+h,x:x+w]
-        # #-- In each face, detect eyes
-        # eyes = eyes_cascade.detectMultiScale(faceROI)
-        # for (x2,y2,w2,h2) in eyes:
-        #     eye_center = (x + x2 + w2//2, y + y2 + h2//2)
-        #     radius = int(round((w2 + h2)*0.25))
-        #     frame = cv.circle(frame, eye_center, radius, (255, 0, 0 ), 4)
-    cv.imshow('Capture - Face detection', frame)
-    
+def draw_on_frame(frame, faces, fps, thickness=2):
+    if faces[1] is not None:
+        for idx, face in enumerate(faces[1]):
+            print('Face {}, top-left coordinates: ({:.0f}, {:.0f}), box width: {:.0f}, box height {:.0f}, score: {:.2f}'.format(idx, face[0], face[1], face[2], face[3], face[-1]))
+            coords = face[:-1].astype(np.int32)
+            cv.rectangle(frame, (coords[0], coords[1]), (coords[0]+coords[2], coords[1]+coords[3]), (0, 255, 0), thickness)
+            # cv.circle(frame, (coords[4], coords[5]), 2, (255, 0, 0), thickness)
+            # cv.circle(frame, (coords[6], coords[7]), 2, (0, 0, 255), thickness)
+            # cv.circle(frame, (coords[8], coords[9]), 2, (0, 255, 0), thickness)
+            # cv.circle(frame, (coords[10], coords[11]), 2, (255, 0, 255), thickness)
+            # cv.circle(frame, (coords[12], coords[13]), 2, (0, 255, 255), thickness)
+    cv.putText(frame, 'FPS: {:.2f}'.format(fps), (1, 16), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
     
 def main():
-    # parser = argparse.ArgumentParser(description='Code for Cascade Classifier tutorial.')
-    # parser.add_argument('--face_cascade', help='Path to face cascade.', default='data/haarcascades/haarcascade_frontalface_alt.xml')
-    # parser.add_argument('--eyes_cascade', help='Path to eyes cascade.', default='data/haarcascades/haarcascade_eye_tree_eyeglasses.xml')
-    # parser.add_argument('--camera', help='Camera divide number.', type=int, default=0)
-    # args = parser.parse_args()
-    # face_cascade_name = args.face_cascade
-    # eyes_cascade_name = args.eyes_cascade
-    face_cascade_name = 'Data/haarcascade_frontalface_alt.xml'
-    face_cascade = cv.CascadeClassifier()
-    face_cascade.load(face_cascade_name)
-    # #-- 1. Load the cascades
-    # if not face_cascade.load(cv.samples.findFile(face_cascade_name)):
-    #     print('--(!)Error loading face cascade')
-    #     exit(0)
-
-    process_webcam(face_cascade)
+    tm = cv.TickMeter()
+    # Initialize detector
+    detector = cv.FaceDetectorYN.create(
+        "Data/face_detection_yunet_2022mar.onnx", 
+        "", 
+        (320, 320),
+        0.9,
+        0.3,
+        5000)
+    process_webcam(detector, tm)
 
 if __name__ == "__main__":
     main()
