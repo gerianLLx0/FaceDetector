421f2bbf8133ba86ab1662703ad4e380fa3f59a9
x0g <gerianllanes@gmail.com>
2023-04-08 23:07:05 +1000

    add number of faces counter

--- face_detector.py
+++ face_detector.py
@@ -38,6 +38,7 @@ def process_webcam(detector, tm):
         if not ret:
             print("Can't receive frame (stream end?). Exiting ...")
             break
+        
         # Operations on frame
         img_H = int(frame.shape[0])
         img_W = int(frame.shape[1])
@@ -49,17 +50,20 @@ def process_webcam(detector, tm):
         # Draw results on the input image
         draw_on_frame(frame, faces, tm.getFPS())
         cv.imshow('Live', frame)
+        
         # How to exit
         if cv.waitKey(1) == ord('q'):
             break
     # When done, release the capture
     cap.release()
     cv.destroyAllWindows()
+
     
 def draw_on_frame(frame, faces, fps, thickness=2):
     if faces[1] is not None:
+        num_faces = len(faces[1])
         for idx, face in enumerate(faces[1]):
-            print('Face {}, top-left coordinates: ({:.0f}, {:.0f}), box width: {:.0f}, box height {:.0f}, score: {:.2f}'.format(idx, face[0], face[1], face[2], face[3], face[-1]))
+            # print('Face {}, top-left coordinates: ({:.0f}, {:.0f}), box width: {:.0f}, box height {:.0f}, score: {:.2f}'.format(idx, face[0], face[1], face[2], face[3], face[-1]))
             coords = face[:-1].astype(np.int32)
             cv.rectangle(frame, (coords[0], coords[1]), (coords[0]+coords[2], coords[1]+coords[3]), (0, 255, 0), thickness)
             # cv.circle(frame, (coords[4], coords[5]), 2, (255, 0, 0), thickness)
@@ -67,8 +71,9 @@ def draw_on_frame(frame, faces, fps, thickness=2):
             # cv.circle(frame, (coords[8], coords[9]), 2, (0, 255, 0), thickness)
             # cv.circle(frame, (coords[10], coords[11]), 2, (255, 0, 255), thickness)
             # cv.circle(frame, (coords[12], coords[13]), 2, (0, 255, 255), thickness)
-    cv.putText(frame, 'FPS: {:.2f}'.format(fps), (1, 16), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-    
+        cv.putText(frame, 'FPS: {:.2f}'.format(fps), (1, 16), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        cv.putText(frame, f'Number of Faces: {num_faces}', (1, 50), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
 def main():
     tm = cv.TickMeter()
     # Initialize detector
